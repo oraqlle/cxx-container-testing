09c6285f3bbb7dcf93201f60c5729b3ef3c8f446
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-05-30 07:26:34 +1000

    Changed CLI library to `argparse`. Updated CLI logic.

--- include/cli.hpp
+++ include/cli.hpp
@@ -4,8 +4,6 @@
 #include <runners.hpp>
 #include <types.hpp>
 
-#include <cxxopts.hpp>
-
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 #include <fmt/std.h>
@@ -13,6 +11,7 @@
 #include <array>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace cli {
 
@@ -25,60 +24,51 @@ constexpr std::array<std::string_view, 4> test_names = {
     "random-insert - Tests containers ability to insert elements into a random position."sv
 };
 
-auto run_tests(cxxopts::ParseResult& args) noexcept -> void
+auto run_tests(const std::vector<std::string>& test_args) noexcept -> void
 {
-    try {
-        auto test_args = args["test"].as<std::vector<std::string>>();
+    for (auto& test_arg : test_args) {
+        if (test_arg == "all"sv) {
+            runners::all<
+                types::TrivialSmallType,
+                types::TrivialMediumType,
+                types::TrivialLargeType,
+                types::TrivialHugeType,
+                types::TrivialMonsterType>();
 
-        for (auto& test_arg : test_args) {
-            if (test_arg == "all"sv) {
-                runners::all<
-                    types::TrivialSmallType,
-                    types::TrivialMediumType,
-                    types::TrivialLargeType,
-                    types::TrivialHugeType,
-                    types::TrivialMonsterType>();
-
-                break;
-            } else if (test_arg == "push-back") {
-                runners::run_for_types<
-                    runners::PushBack,
-                    types::TrivialSmallType,
-                    types::TrivialMediumType,
-                    types::TrivialLargeType,
-                    types::TrivialHugeType,
-                    types::TrivialMonsterType>();
-            } else if (test_arg == "linear-search"sv) {
-                runners::run_for_types<
-                    runners::LinearSearch,
-                    types::TrivialSmallType,
-                    types::TrivialMediumType,
-                    types::TrivialLargeType,
-                    types::TrivialHugeType,
-                    types::TrivialMonsterType>();
-            } else if (test_arg == "random-insert"sv) {
-                runners::run_for_types<
-                    runners::RandomInsert,
-                    types::TrivialSmallType,
-                    types::TrivialMediumType,
-                    types::TrivialLargeType,
-                    types::TrivialHugeType,
-                    types::TrivialMonsterType>();
-            }
+            break;
+        } else if (test_arg == "push-back") {
+            runners::run_for_types<
+                runners::PushBack,
+                types::TrivialSmallType,
+                types::TrivialMediumType,
+                types::TrivialLargeType,
+                types::TrivialHugeType,
+                types::TrivialMonsterType>();
+        } else if (test_arg == "linear-search"sv) {
+            runners::run_for_types<
+                runners::LinearSearch,
+                types::TrivialSmallType,
+                types::TrivialMediumType,
+                types::TrivialLargeType,
+                types::TrivialHugeType,
+                types::TrivialMonsterType>();
+        } else if (test_arg == "random-insert"sv) {
+            runners::run_for_types<
+                runners::RandomInsert,
+                types::TrivialSmallType,
+                types::TrivialMediumType,
+                types::TrivialLargeType,
+                types::TrivialHugeType,
+                types::TrivialMonsterType>();
         }
-    } catch (cxxopts::exceptions::option_has_no_value& e) {
-        return;
     }
 }
 
-auto list_tests(cxxopts::ParseResult& args) noexcept -> void
+auto list_tests() noexcept -> void
 {
-    if (args["list"].as<bool>()) {
-
-        fmt::print(
-            "C++ Container Tester\n\nAvailable Tests:\n{}\n\n",
-            fmt::join(test_names, "\n"sv));
-    }
+    fmt::print(
+        "C++ Container Tester\n\nAvailable Tests:\n{}\n\n",
+        fmt::join(test_names, "\n"sv));
 }
 
 } // namespace cli
--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,51 @@
 
 #include <cli.hpp>
 
-#include <cxxopts.hpp>
+#include <argparse/argparse.hpp>
 
+#include <cstdlib>
+#include <exception>
+#include <ostream>
 #include <string>
-#include <string_view>
 #include <vector>
 
 using namespace std::literals;
 
 int main(int argc, char* argv[])
 {
-    auto cli_options = cxxopts::Options {
-        "C++ Container Testing",
-        "A simple program for benchmarking the performance off C++ standard containers."
-    };
+    auto program = argparse::ArgumentParser { "cxx-container-testing", "v0.1.0" };
 
-    cli_options.add_options()(
-        "t,test",
-        "Run a particular test",
-        cxxopts::value<std::vector<std::string>>()->implicit_value("all"));
+    program.add_description("Tests a variety of different fundamental operations on C++ Containers");
 
-    cli_options.add_options()(
-        "l,list",
-        "List all available tests",
-        cxxopts::value<bool>()->default_value("false"));
+    program.add_argument("-l", "--list")
+        .help("Lists all available tests")
+        .default_value(false)
+        .implicit_value(true);
 
-    auto args = cli_options.parse(argc, argv);
+    program.add_argument("-t", "--test")
+        .help("Run a particular test")
+        .append();
 
-    cli::list_tests(args);
-    cli::run_tests(args);
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    if (program["--list"] == true) {
+        cli::list_tests();
+        std::exit(0);
+    }
+
+    auto tests = program.get<std::vector<std::string>>("--test");
+
+    if (tests.empty())
+        tests.push_back("all"s);
+
+    cli::run_tests(tests);
+    std::exit(0);
 
     return 0;
 }
